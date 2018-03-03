3359c4af7aaa867454b4886a8e9a6fd4ab330098
Ashley Baldry <Ashley.Baldry@kantarmillwardbrown.co>
2018-03-03 19:46:32 +0000

    Sorting out R6 object functions

--- R/account.R
+++ R/account.R
@@ -4,69 +4,133 @@
 #' @field pass password for the Visual Studio account
 #' @field domain domain name for the Visual Studio account
 #' @field project (optional) project name within the domain of the Visual Studio account
+#' @field repo (optional) repository name with the project of the Visual Studio domain
 #'
 #' @docType class
 #' @format An \code{\link{R6Class}} generator object
 #' @keywords data
 #'
 #' @examples
-#' proj <- vsts_account$new('<username>', '<password>', '<domain name>', '<project name>')
+#' proj <- vsts_account$new('<username>', '<password>', '<domain name>', '<project name>', '<repo name>')
 #' str(proj)
 #'
 #' @export
-vsts_account <- R6::R6Class(classname = 'vsts',
-                            public = list(
-                              user = NULL,
-                              domain = NULL,
-                              project = NULL,
+vsts_account <- R6::R6Class(
+  classname = 'vsts',
+  public = list(
+    initialize = function(user = NA, pass = NA, domain = NA, project = NULL, repo = NULL) {
+      private$user <- user
+      private$pass <- pass
+      private$auth_key <- private$get_auth_key(user, pass)
+      private$domain <- domain
+      private$projects <- vsts_get_projects(private$domain, private$auth_key, quiet = TRUE)$name
+      if(!is.null(project)) {
+        if(!project %in% private$projects) stop(project, ' is not available in selected domain.')
+        private$project <- project
+        private$repos <- vsts_get_repos(private$domain, private$project, private$auth_key, quiet = TRUE)$name
+      }
+      if(!is.null(repo)) {
+        if(is.null(project)) stop('`project` needs to be specified to include repository.')
+        if(!repo %in% private$repos) stop(repo, ' is not available in selected project.')
+        private$repo <- repo
+      }
+    },
 
-                              initialize = function(user = NA, pass = NA, domain = NA, project = NULL) {
-                                self$user <- user
-                                self$domain <- domain
-                                self$project <- project
-                                private$pass <- pass
-                                private$auth_key <- private$get_auth_key(user, pass)
-                              },
+    set_domain = function(dom) {
+      private$domain <- dom
+      proj_df <- vsts_get_projects(private$domain, private$auth_key, quiet = TRUE)
+      private$projects <- proj_df$name
+      invisible(TRUE)
+    },
 
-                              get_projects = function() vsts_get_projects(self$domain, private$auth_key),
+    set_project = function(proj) {
+      if(is.null(private$projects)) {
+        proj_df <- vsts_get_projects(private$domain, private$auth_key, quiet = TRUE)
+        private$projects <- proj_df$name
+      }
+      if(!proj %in% private$projects) stop(proj, ' not in available projects.')
+      private$project <- proj
 
-                              get_repos = function() {
-                                private$proj_check()
-                                vsts_get_repos(self$domain, self$project, private$auth_key)
-                              },
-                              create_repo = function(repo) {
-                                private$proj_check()
-                                vsts_create_repo(self$domain, self$project, repo, private$auth_key)
-                              },
-                              delete_repo = function(repo) {
-                                private$proj_check()
-                                vsts_delete_repo(self$domain, self$project, repo, private$auth_key)
-                              },
+      repo_df <- vsts_get_repos(private$domain, private$project, private$auth_key, quiet = TRUE)
+      private$repos <- repo_df$name
+      invisible(TRUE)
+    },
 
-                              get_commits = function(repo, query = NULL) {
-                                vsts_get_commits(self$domain, self$project, repo, private$auth_key, query = query)
-                              },
+    set_repo = function(repo) {
+      if(is.null(private$repos)) {
+        repo_df <- vsts_get_repos(private$domain, private$project, private$auth_key, quiet = TRUE)
+        private$repos <- repo_df$name
+      }
+      if(!repo %in% private$projects) stop(repo, ' not in available repositories.')
+      private$repo <- repo
+      invisible(TRUE)
+    },
 
-                              get_workitems = function(query = NULL) {
-                                vsts_get_workitems(self$domain, private$auth_key, query = query)
-                              },
-                              get_workitem = function(id) {
-                                vsts_get_workitem(self$domain, private$auth_key, id = id)
-                              },
-                              create_workitem = function(item_type, ...) {
-                                vsts_create_workitem(self$domain, self$project, item_type, private$auth_key, ...)
-                              },
+    get_projects = function() vsts_get_projects(private$domain, private$auth_key),
 
-                              custom_command = function(url, verb, body = NULL, query = NULL) {
-                                vsts_run_command(url, verb, private$auth_key, body, query)
-                              }
+    get_repos = function() {
+      private$proj_check()
+      vsts_get_repos(private$domain, private$project, private$auth_key)
+    },
 
-                            ),
+    create_repo = function(repo) {
+      private$proj_check()
+      if(repo %in% private$repos) stop(repo, ' already exists in ', private$project, '.')
+      content <- vsts_create_repo(private$domain, private$project, repo, private$auth_key)
+      private$repos <- vsts_get_repos(private$domain, private$project, private$auth_key, quiet = TRUE)$name
+      invisible(content)
+    },
 
-                            private = list(
-                              pass = NULL,
-                              auth_key = NULL,
+    delete_repo = function(repo) {
+      private$proj_check()
+      if(!repo %in% private$repos) stop(repo, ' does not exist in ', private$project, '.')
+      content <- vsts_delete_repo(private$domain, private$project, repo, private$auth_key)
+      private$repos <- vsts_get_repos(private$domain, private$project, private$auth_key, quiet = TRUE)$name
+      invisible(content)
+    },
 
-                              get_auth_key = function(user, pass) vsts_auth_key(user, pass),
-                              proj_check = function() if(is.null(self$project)) stop('Project name must be included in \'project\'')
-                            ))
+    get_commits = function(query = NULL) {
+      private$repo_check()
+      vsts_get_commits(private$domain, private$project, private$repo, private$auth_key, query = query)
+    },
+
+    get_workitems = function(query = NULL) {
+      vsts_get_workitems(private$domain, private$auth_key, query = query)
+    },
+
+    get_workitem = function(id) {
+      vsts_get_workitem(private$domain, private$auth_key, id = id)
+    },
+
+    create_workitem = function(item_type, ...) {
+      private$proj_check()
+      vsts_create_workitem(private$domain, private$project, item_type, private$auth_key, ...)
+    },
+
+    custom_command = function(url, verb, body = NULL, query = NULL) {
+      vsts_run_command(url, verb, private$auth_key, body, query)
+    }
+
+  ),
+
+  private = list(
+    # Making sure these are not easily editable
+    user = NULL,
+    domain = NULL,
+    project = NULL,
+    repo = NULL,
+    pass = NULL,
+    auth_key = NULL,
+    projects = NULL,
+    repos = NULL,
+
+    get_auth_key = function(user, pass) vsts_auth_key(user, pass),
+
+    proj_check = function() {
+      if(is.null(private$project)) stop('Project name must be added to object. Use `$set_project()`')
+    },
+    repo_check = function() {
+      if(is.null(private$repo)) stop('Repository name must be added to object. Use `$set_repo()`')
+    }
+  )
+)
